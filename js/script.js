3d0372a73853b01702d3bc6dceafa6a001536ab8
Marco <marco.cusenza.me@gmail.com>
2021-11-09 18:02:33 +0100

    milestone 3

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const grid = document.getElementById("grid");
 let x = 0;
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
     if (i % 15 == 0) {
         x = "FizzBuzz";
     } else if (i % 3 == 0) {
